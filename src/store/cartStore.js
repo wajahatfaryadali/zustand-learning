3318aea7de6e15efaeeb6df2887bdfeeadc4defc
wajahat ali <wajahatfaryadali@gmail.com>
2025-02-13 19:16:38 +0500

    cart store setup

---
+++ src/store/cartStore.js
@@ -0,0 +1,8 @@
+import { create } from 'zustand'
+
+export const useCartStore = create((set) => ({
+    cart: [],
+    addToCart: (product) => set((state) => ({ cart: [state.cart, product] })),
+    removeFromCart: (productID) => set((state) => ({ cart: state.cart.filter(prod => prod.id !== productID) })),
+    clearCart: () => set(state => ({ cart: [] }))
+}))
